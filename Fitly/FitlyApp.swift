2c515d87e66ebe962b507a5ff290b9ac9ddef100
İrem Sever <iremsever_@outlook.com>
2024-09-03 14:28:47 +0300

    create app

---
+++ Fitly/FitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitlyApp.swift
+//  Fitly
+//
+//  Created by İrem Sever on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
